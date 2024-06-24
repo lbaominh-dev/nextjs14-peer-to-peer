3839951fa23701f607ce914ac110662f89aebf7b
Minh Luu <lbaominh.dev@gmail.com>
2024-06-24 15:34:05 +0700

    chore: update VideoChat config path

--- modules/VideoChat/config.ts
+++ modules/VideoChat/config.ts
@@ -1,5 +1,5 @@
 export const peerOptions = {
   host: process.env.NEXT_PUBLIC_PEERJS_HOST ?? window.location.hostname,
-  path: process.env.NEXT_PUBLIC_PEERJS_PATH ?? "/",
-  port: parseInt(process.env.NEXT_PUBLIC_PEERJS_PORT ?? "9000"),
+  path: process.env.NEXT_PUBLIC_PEERJS_PATH ?? "/peerjs",
+//   port: parseInt(process.env.NEXT_PUBLIC_PEERJS_PORT ?? "9000"),
 };
